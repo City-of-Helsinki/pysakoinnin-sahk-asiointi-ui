5e2ac3ddc4dbeb720b02e4d180c60d18f08e62db
Veera Alt <veera.alt@aalto.fi>
2022-10-28 13:47:33 +0300

    Create first form view (#5)
    * Add layout for the first view of parking fine appeal form

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { ClientContext } from '../client/ClientProvider';
 import LoginComponent from '../components/Login';
+import SearchForm from '../components/searchForm/SearchForm';
 import PageContent from '../components/PageContent';
 import ReduxConsumer from '../components/ReduxConsumer';
 import WithAuthDemo from '../components/WithAuthDemo';
 import ClientConsumer from '../components/ClientConsumer';
 import { getClientConfig } from '../client';
+import { StoreState } from '../client/redux/index';
+import { Button } from 'hds-react';
 
 const Index = (): React.ReactElement => {
   const currentConfig = getClientConfig();
   const clientContext = useContext(ClientContext);
+  const state: StoreState = useSelector((storeState: StoreState) => storeState);
+  const user = state?.user;
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <PageContent>
       {!!clientContext && clientContext.client ? (
@@ -27,6 +35,14 @@ const Index = (): React.ReactElement => {
             headerista.
           </p>
           <p>Voit myös kirjatua ulos toisessa ikkunassa.</p>
+          {user && (
+            <div>
+              <Button onClick={() => setShowForm(!showForm)} variant="primary">
+                {showForm ? 'Sulje lomake' : 'Avaa lomake'}
+              </Button>
+              {showForm && <SearchForm />}
+            </div>
+          )}
           <LoginComponent />
           <ReduxConsumer />
           <WithAuthDemo />
