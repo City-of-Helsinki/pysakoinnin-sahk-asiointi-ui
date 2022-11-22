ef379d525623469d7c663f45fc8757e6303bde23
jihanm <jaakko.ihanamaki@futurice.com>
2022-11-22 14:15:18 +0200

    Fix tests

--- src/components/parkingFineSummary/ParkingFineSummary.test.tsx
+++ src/components/parkingFineSummary/ParkingFineSummary.test.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 import ParkingFineSummary from './ParkingFineSummary';
+import { Provider } from 'react-redux';
+import store from '../../store';
 
 describe('Component', () => {
   it('matches snapshot', () => {
-    const { container } = render(<ParkingFineSummary />);
+    const { container } = render(
+      <Provider store={store}>
+        <ParkingFineSummary />
+      </Provider>
+    );
     expect(container).toMatchSnapshot();
   });
 
   it('passes A11y checks', async () => {
-    const { container } = render(<ParkingFineSummary />);
+    const { container } = render(
+      <Provider store={store}>
+        <ParkingFineSummary />
+      </Provider>
+    );
 
     expect(await axe(container)).toHaveNoViolations();
   });
