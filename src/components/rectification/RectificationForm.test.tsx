b96e3a7f49052dbfeb6c2c5be09887e2e73b874c
Veera Alt <veera.alt@aalto.fi>
2022-12-01 18:12:38 +0200

    Update tests

--- src/components/rectification/RectificationForm.test.tsx
+++ src/components/rectification/RectificationForm.test.tsx
@@ -8,12 +8,50 @@ import { axe } from 'jest-axe';
 // eslint-disable-next-line no-magic-numbers
 jest.setTimeout(10000);
 
-describe('Component', () => {
+describe('Component in parking fine appeal form', () => {
   const formContentSliceMock = createSlice({
     name: 'formContent',
     initialState: {
       formSubmitted: false,
       selectedForm: 'parking-fine',
+      submitDisabled: true
+    },
+    reducers: {}
+  });
+
+  const store = configureStore({
+    reducer: {
+      formContent: formContentSliceMock.reducer
+    }
+  });
+
+  it('matches snapshot', async () => {
+    const { container } = render(
+      <Provider store={store}>
+        <RectificationForm />
+      </Provider>
+    );
+
+    await waitFor(() => expect(container).toMatchSnapshot());
+  });
+
+  it('passes A11y checks', async () => {
+    const { container } = render(
+      <Provider store={store}>
+        <RectificationForm />
+      </Provider>
+    );
+
+    expect(await axe(container)).toHaveNoViolations();
+  });
+});
+
+describe('Component in moved car form', () => {
+  const formContentSliceMock = createSlice({
+    name: 'formContent',
+    initialState: {
+      formSubmitted: false,
+      selectedForm: 'moved-car',
       submitDisabled: true
     },
     reducers: {}
