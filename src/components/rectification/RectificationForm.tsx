cd75f24c92297749449eda57ac1f703c75277411
Veera Alt <veera.alt@aalto.fi>
2022-12-01 18:42:41 +0200

    Add 'save draft' button

--- src/components/rectification/RectificationForm.tsx
+++ src/components/rectification/RectificationForm.tsx
@@ -3,10 +3,13 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import {
+  Button,
   Checkbox,
   FileInput,
   IconCheckCircle,
+  IconUploadCloud,
   Link,
+  Notification,
   RadioButton,
   Select,
   SelectionGroup,
@@ -32,6 +35,9 @@ const RectificationForm = () => {
   const [newEmailSelected, setNewEmailSelected] = useState(false);
   const [vehicleRelation, setVehicleRelation] = useState('driver');
   const [currentCharacters, setCurrentCharacters] = useState(0);
+  const [showDraftSavedNotification, setShowDraftSavedNotification] = useState(
+    false
+  );
 
   const [decision, setDecision] = useState('toParkingService');
 
@@ -46,6 +52,10 @@ const RectificationForm = () => {
 
   const handleDecision = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDecision(e.target.value);
+  };
+
+  const handleDraftSave = () => {
+    setShowDraftSavedNotification(true);
   };
 
   const relations = movedCarFormSelected
@@ -245,6 +255,27 @@ const RectificationForm = () => {
               checked={decision === 'byMail'}
             />
           </SelectionGroup>
+        </div>
+        <div>
+          <Button
+            className="rectification-draft-button"
+            iconLeft={<IconUploadCloud />}
+            onClick={handleDraftSave}
+            variant="secondary">
+            {t('common:save-draft')}
+          </Button>
+          {showDraftSavedNotification && (
+            <Notification
+              label={t('common:notifications:draft-saved:label')}
+              position="bottom-right"
+              type={'success'}
+              autoClose
+              dismissible
+              closeButtonLabelText="Close notification"
+              onClose={() => setShowDraftSavedNotification(false)}>
+              {t('common:notifications:draft-saved:text')}
+            </Notification>
+          )}
         </div>
       </div>
     </>
