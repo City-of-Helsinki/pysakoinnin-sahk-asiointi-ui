861a6d12ea7af81238482724ae29f8f649467077
Veera Alt <veera.alt@aalto.fi>
2022-12-19 17:08:44 +0200

    Fix summary view of moved car appeal form (#38)

--- src/components/rectificationSummary/RectificationSummary.test.tsx
+++ src/components/rectificationSummary/RectificationSummary.test.tsx
@@ -4,9 +4,25 @@ import { axe } from 'jest-axe';
 import RectificationSummary from './RectificationSummary';
 import store from '../../store';
 import { Provider } from 'react-redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
 describe('Component', () => {
   it('matches snapshot', () => {
+    const formContentSliceMock = createSlice({
+      name: 'formContent',
+      initialState: {
+        formSubmitted: false,
+        selectedForm: 'parking-fine',
+        submitDisabled: true
+      },
+      reducers: {}
+    });
+
+    const store = configureStore({
+      reducer: {
+        formContent: formContentSliceMock.reducer
+      }
+    });
     const { container } = render(
       <Provider store={store}>
         <RectificationSummary />
