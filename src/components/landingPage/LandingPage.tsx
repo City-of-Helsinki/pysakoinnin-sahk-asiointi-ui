7d6e84ea59e9838f65b67268f7ffdd768274a101
Veera Alt <veera.alt@aalto.fi>
2023-02-20 13:05:16 +0200

    Add pagination

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Linkbox } from 'hds-react';
+import React, { useRef, useState, MouseEvent } from 'react';
+import { Linkbox, Pagination } from 'hds-react';
 import { useTranslation } from 'react-i18next';
 import RectificationListRow from '../rectificationListRow/RectificationListRow';
 import './LandingPage.css';
@@ -7,11 +7,20 @@ import './LandingPage.css';
 
 const LandingPage = (): React.ReactElement => {
   const { t } = useTranslation();
+  const [pageIndex, setPageIndex] = useState(0);
+  const elementsOnPage = 5;
+  const titleRef = useRef<null | HTMLDivElement>(null);
   const links = [
     { name: 'due-date', value: '/erapaivansiirto' },
     { name: 'parking-fine', value: '/virhemaksu' },
     { name: 'moved-car', value: '/ajoneuvonsiirto' }
   ];
+
+  const handlePageChange = (event: MouseEvent, index: number) => {
+    event.preventDefault();
+    setPageIndex(index);
+    titleRef.current?.scrollIntoView();
+  };
 
   const rectificationForms = [
     {
@@ -43,8 +52,21 @@ const LandingPage = (): React.ReactElement => {
       type: 'moved-car',
       status: 'solved-online',
       edited: '2023-02-01'
+    },
+    {
+      id: 67890123,
+      type: 'parking-fine',
+      status: 'processing',
+      edited: '2023-02-11'
+    },
+    {
+      id: 78901234,
+      type: 'due-date',
+      status: 'solved-online',
+      edited: '2022-11-20'
     }
   ];
+
   return (
     <>
       <div className="landing-page-link-list">
@@ -61,16 +83,33 @@ const LandingPage = (): React.ReactElement => {
           />
         ))}
       </div>
-      <h2>{t('landing-page:list:title')}</h2>
+      <h2 ref={titleRef}>{t('landing-page:list:title')}</h2>
+      <p>
+        <b>{rectificationForms.length}</b> {t('landing-page:sent')}
+      </p>
       <div className="rectification-list">
         <hr />
-        {rectificationForms.map(form => (
-          <div key={form.id}>
-            <RectificationListRow form={form} />
-            <hr />
-          </div>
-        ))}
+        {rectificationForms
+          .slice(
+            pageIndex * elementsOnPage,
+            pageIndex * elementsOnPage + elementsOnPage
+          )
+          .map(form => (
+            <div key={form.id}>
+              <RectificationListRow form={form} />
+              <hr />
+            </div>
+          ))}
       </div>
+      <Pagination
+        language="fi"
+        onChange={(event, index) => handlePageChange(event, index)}
+        pageCount={Math.ceil(rectificationForms.length / elementsOnPage)}
+        pageHref={() => '#'}
+        pageIndex={pageIndex}
+        paginationAriaLabel="pagination"
+        siblingCount={2}
+      />
     </>
   );
 };
