505ef26f0d598fc2429815c8fd7a7962e14b7737
Jaakko Ihanamäki <84900656+jihanm@users.noreply.github.com>
2023-05-16 13:03:45 +0300

    Add english translations and language switcher (#92)

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -4,6 +4,10 @@ import styles from './styles.module.css';
 
 const PageContent = (
   props: React.PropsWithChildren<unknown>
-): React.ReactElement => <div className={styles.content}>{props.children}</div>;
+): React.ReactElement => (
+  <div className={styles.content} id="content">
+    {props.children}
+  </div>
+);
 
 export default PageContent;
