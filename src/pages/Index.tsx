271a508e50dba069845f5ed31ffd84c575145f34
henri-hietala <henri.hietala@futurice.com>
2023-06-21 12:34:32 +0300

    use user default lang if localstore is empty

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ClientContext } from '../client/ClientProvider';
 import LoginComponent from '../components/Login';
 import PageContent from '../components/PageContent';
@@ -7,6 +7,8 @@ import useUserProfile from '../hooks/useUserProfile';
 import { UserProfile } from '../common';
 import { GraphQLClientError } from '../graphql/graphqlClient';
 import { useTranslation } from 'react-i18next';
+import i18n from '../utils/i18n';
+// import i18n from '../utils/i18n';
 
 const Index = (): React.ReactElement => {
   const clientContext = useContext(ClientContext);
@@ -15,9 +17,38 @@ const Index = (): React.ReactElement => {
   // to a new page, thus refreshing the store. The user is re-fetched and added to redux in FormStepper.tsx
   const userProfile = useUserProfile() as UserProfile | GraphQLClientError;
 
+  console.log(localStorage.getItem('lang') == null);
+  console.log(localStorage.getItem('lang'));
+
   const isUser: boolean =
     userProfile &&
     Object.prototype.hasOwnProperty.call(userProfile, 'firstName');
+
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+    localStorage.setItem('lang', lang);
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('lang') == null && isUser) {
+      const user = userProfile as UserProfile;
+      let defaultLang = null;
+
+      switch (user.language) {
+        case 'FINNISH':
+          defaultLang = 'fi';
+          break;
+        case 'SWEDISH':
+          defaultLang = 'sv';
+          break;
+        case 'ENGLISH':
+          defaultLang = 'en';
+          break;
+      }
+      if (defaultLang) changeLanguage(defaultLang);
+    }
+  }, [isUser]);
 
   return (
     <PageContent>
