8a02c9c5a70c163dd34859d3159f3cd8e4f3b9f2
mikkojamG <mikko.jamia@gofore.com>
2024-05-28 14:45:59 +0300

    deps: remove enzyme RATY-116

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { MemoryRouter } from 'react-router';
-
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  shallow(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  );
+import renderWithProvider from './utils/renderWithProviders';
+import { act } from '@testing-library/react';
+
+it('renders without crashing', async () => {
+  await act(async () => {
+    renderWithProvider(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+  });
 });
